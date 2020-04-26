a5c98681ce0f2f60909feb19be0d30933606c313
gunkim0318 <gunkim0318@gmail.com>
2020-04-26 09:23:29 +0900

    스프링 시큐리티 적용 중

---
+++ src/main/java/application/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                    .loginPage("/member/signIn")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .permitAll();
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+    }
+}
