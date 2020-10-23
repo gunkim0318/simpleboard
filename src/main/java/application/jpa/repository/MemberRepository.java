3234eabc7bfacbf5e93e07786b1df568c0b0985e
gunkim <gunkim0318@gmail.com>
2020-10-23 22:56:46 +0900

    Nickname으로도 유저를 검색할 수 있도록 메소드 추가

--- src/main/java/application/jpa/repository/MemberRepository.java
+++ src/main/java/application/jpa/repository/MemberRepository.java
@@ -3,9 +3,12 @@ package application.jpa.repository;
 import application.jpa.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Member 저장소
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String name);
 }
